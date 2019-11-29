fe4f91bc3c249ca570bba2f5d3683558bbb817e9
axthomas29 <axthomas29@gmail.com>
2019-11-29 14:58:01 -0500

    added loops to case study img container

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -23,7 +23,8 @@ get_header(); ?>
             $link = get_field('site_link');
             $image_1 = get_field('image_1');
             $image_2 = get_field('image_2');
-            $image_3 = get_field('image_3'); ?>
+            $image_3 = get_field('image_3');
+            $size = "full"; ?>
             <article class="case-study">
                 <aside class="case-study-sidebar">
                     <h2><?php the_title(); ?></h2>
@@ -37,9 +38,17 @@ get_header(); ?>
                 </aside>
 
                 <div class="case-study-img-container">
-                    <img src="<?php echo $image_1; ?>" />
-                    <img src="<?php echo $image_2; ?>" />
-                    <img src="<?php echo $image_3; ?>" />
+                    <?php if ($image_1) {
+                            echo wp_get_attachment_image($image_1, $size);
+                        } ?>
+
+                    <?php if ($image_2) {
+                            echo wp_get_attachment_image($image_2, $size);
+                        } ?>
+
+                    <?php if ($image_3) {
+                            echo wp_get_attachment_image($image_3, $size);
+                        } ?>
                 </div>
             </article>
 
