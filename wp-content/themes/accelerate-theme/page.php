2ea30fae76f2987b6a195b941da8c6eaddfadb8a
axthomas29 <axthomas29@gmail.com>
2019-11-29 11:16:51 -0500

    adding custom post type and CSS styles

--- wp-content/themes/accelerate-theme/page.php
+++ wp-content/themes/accelerate-theme/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,15 +15,16 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+<div id="primary" class="site-content sidebar">
+	<div class="main-content" role="main">
+		<?php while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; // end of the loop. 
+		?>
+	</div><!-- .main-content -->
 
-		<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 
-	</div><!-- #primary -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
