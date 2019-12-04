389402cee52872eafbd3f8243f9a57d58fa967d4
axthomas29 <axthomas29@gmail.com>
2019-12-04 15:30:20 -0500

    added about headline and CSS

--- wp-content/themes/accelerate-theme-child/about.php
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -3,6 +3,9 @@
 <?php get_header(); ?>
 
 <div id="primary" class="home-page hero-content">
+    <div class="hero-about-container">
+        <h2>Accelerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h2>
+    </div>
     <div class="main-content" role="main">
         <?php while (have_posts()) : the_post(); ?>
             <?php the_content(); ?>
